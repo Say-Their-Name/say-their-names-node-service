b2370281380a37dead8e164265e18ceeb730400b
Nedim <nedx45@gmail.com>
2020-06-12 15:53:56 +0200

    fix AASA file path

--- server/api/routes/aasa.js
+++ server/api/routes/aasa.js
@@ -2,18 +2,20 @@
 
 const express = require('express')
 const fs = require('fs')
+const path = require('path')
 const router = express.Router()
 
 router.get('/apple-app-site-association', function (req, res) {
-   
-    var aasa = fs.readFileSync('./public/apple-app-site-association.json')
+
+
+    var aasa = fs.readFileSync(path.resolve(__dirname,'../public/apple-app-site-association.json'))
         res.set('Content-Type', 'application/pkcs7-mime')
         res.status(200).send(aasa)
 })
 
 router.get('/.well-known/apple-app-site-association', function (req, res) {
    
-    var aasa = fs.readFileSync('./public/apple-app-site-association.json')
+    var aasa = fs.readFileSync(path.resolve(__dirname,'../public/apple-app-site-association.json'))
         res.set('Content-Type', 'application/pkcs7-mime')
         res.status(200).send(aasa)
 })
